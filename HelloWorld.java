15be4439cfb5ca7d08fdb6ffa18db4d5cce1ba12
Vishnu Vijayakumar - c001438 <c001438@footlocker.com>
2018-04-17 11:45:14 -0500

    first commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -21,7 +21,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
-        System.out.println("Hello, Footlocker");
+        System.out.println("Hello, Footlocker - In GitHub");
         }
 
 }
